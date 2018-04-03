1537b019fc4d1d21edc982cd54789ee873242b59
Pluralia <irina@metrix.ai>
2018-04-03 08:12:09 +0300

    do internal Error dir files

---
+++ library/Potoki/Hasql/Error.hs
@@ -0,0 +1,12 @@
+module Potoki.Hasql.Error
+(
+    Error (..),
+    module Potoki.Hasql.Error.Instances,
+    connectionError,
+    sessionError
+)
+where
+
+import Potoki.Hasql.Error.Hasql
+import Potoki.Hasql.Error.Instances
+import Potoki.Hasql.Error.Types
