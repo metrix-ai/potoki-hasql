dec8f8d950033ac3023f776cacf7acdacc10257d
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-05-24 14:29:12 +0800

    Update the Hasql dep

--- library/Potoki/Hasql/Consume.hs
+++ library/Potoki/Hasql/Consume.hs
@@ -1,34 +1,34 @@
 module Potoki.Hasql.Consume
 where
 
-import           Potoki.Hasql.Prelude   
-import qualified Hasql.Connection         as B
-import qualified Hasql.Query              as E
-import qualified Hasql.Session            as D
-import qualified Potoki.Consume           as O
-import           Potoki.Core.Consume
-import qualified Potoki.Core.Fetch        as C
-import qualified Potoki.Transform         as F
+import Potoki.Hasql.Prelude   
+import Potoki.Hasql.Error.Types  
+import Potoki.Core.Consume
+import qualified Hasql.Connection as B
+import qualified Hasql.Statement as E
+import qualified Hasql.Session as D
+import qualified Potoki.Consume as O
+import qualified Potoki.Core.Fetch as C
+import qualified Potoki.Transform as F
 import qualified Potoki.Hasql.Error.Hasql as G
-import           Potoki.Hasql.Error.Types  
 
 
-executeBatchQueryConcurrently :: E.Query (Vector params) () -> Int -> Int -> B.Settings -> Consume params (Either Error ())
-executeBatchQueryConcurrently query batchSize amountOfConnections settings =
+executeBatchStatementConcurrently :: E.Statement (Vector params) () -> Int -> Int -> B.Settings -> Consume params (Either Error ())
+executeBatchStatementConcurrently statement batchSize amountOfConnections settings =
   transform batchTransform (right' O.concat)
   where
     batchTransform =
-      F.concurrently amountOfConnections (F.consume (executeBatchQuery query batchSize settings))
+      F.concurrently amountOfConnections (F.consume (executeBatchStatement statement batchSize settings))
 
-executeBatchQuery :: E.Query (Vector params) () -> Int -> B.Settings -> Consume params (Either Error ())
-executeBatchQuery query batchSize settings =
+executeBatchStatement :: E.Statement (Vector params) () -> Int -> B.Settings -> Consume params (Either Error ())
+executeBatchStatement statement batchSize settings =
   transform
     (F.consume (transform (F.take batchSize) O.vector))
-    (executeQuery query settings)
+    (executeStatement statement settings)
 
-executeQuery :: E.Query params () -> B.Settings -> Consume params (Either Error ())
-executeQuery query =
-  executeSession (\ params -> D.query params query)
+executeStatement :: E.Statement params () -> B.Settings -> Consume params (Either Error ())
+executeStatement statement =
+  executeSession (\ params -> D.statement params statement)
 
 executeSession :: (params -> D.Session ()) -> B.Settings -> Consume params (Either Error ())
 executeSession session connectionSettings =
